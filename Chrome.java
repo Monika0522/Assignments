a121c8296b50834c104093d7c6ff0b94fbe57f7f
Monika0522 <monikaqe05@gmail.com>
2024-06-24 21:31:29 +0530

    Add files via upload
    Day 1 assign

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float versionOfType = 91.0f;
+		String developerOftype = "Google";
+		boolean Value= false;
+		int releaseYear = 2008;
+		char shortcutKey ='C';
+		System.out.println("Verision of type = " +versionOfType);
+		System.out.println("Developer type = " +developerOftype);
+		System.out.println("Boolean = " +Value);
+		System.out.println("Yeardisplay = " +releaseYear);
+		System.out.println("shortcut key is = " +shortcutKey);
+
+
+	}
+
+}
